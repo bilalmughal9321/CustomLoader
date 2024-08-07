03cb663740072970b78737e31db8dbd1b131d958
Bilal Mughal <bilal@cinqtech.com>
2024-08-07 15:22:15 +0500

    change some files.

--- Sources/CustomLoader/CustomLoader.swift
+++ Sources/CustomLoader/CustomLoader.swift
@@ -45,37 +45,17 @@ public struct LoaderAnimation: View {
 }
 
 
-struct LoaderAnimationView: UIViewControllerRepresentable {
-    
-    func makeUIViewController(context: Context) -> UIHostingController<LoaderAnimation> {
-        let loaderAnimation = LoaderAnimation()
-        return UIHostingController(rootView: loaderAnimation)
-    }
-    
-    func updateUIViewController(_ uiViewController: UIHostingController<LoaderAnimation>, context: Context) {
-        uiViewController.rootView = LoaderAnimation()
-    }
-}
-
-
-public class CustomLoaderAnimation: UIViewController {
-    public override func viewDidLoad() {
-        super.viewDidLoad()
+public struct LoaderAnimationWrapper {
+    public static func makeLoaderAnimationController(frameWidth: CGFloat, frame: CGRect) -> UIViewController {
+        // Create LoaderAnimation view
+        let loaderAnimationView = LoaderAnimation()
         
-        let loaderAnimation = LoaderAnimationView()
+        // Create UIHostingController with LoaderAnimationView
+        let hostingController = UIHostingController(rootView: loaderAnimationView)
         
-        let hostingController = UIHostingController(rootView: loaderAnimation)
-        addChild(hostingController)
-        view.addSubview(hostingController.view)
-        hostingController.didMove(toParent: self)
+        // Set the frame for the hostingController's view
+        hostingController.view.frame = frame
         
-        // Set constraints or frame for the hostingController's view
-        hostingController.view.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            hostingController.view.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            hostingController.view.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            hostingController.view.widthAnchor.constraint(equalToConstant: 150),
-            hostingController.view.heightAnchor.constraint(equalToConstant: 150)
-        ])
+        return hostingController
     }
 }
