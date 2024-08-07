85cb3dc6dad95b545f4471d237c5752bb31cb881
Bilal Mughal <bilal@cinqtech.com>
2024-08-07 14:58:40 +0500

    add custom spinning loader.

--- Sources/CustomLoader/CustomLoader.swift
+++ Sources/CustomLoader/CustomLoader.swift
@@ -1,2 +1,40 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+
+import SwiftUI
+
+public struct LoaderAnimation: View {
+    @State var isAnimating: Bool = false
+    @State private var animationAmount = 1.0
+    var frameWidth: CGFloat = 100.0
+    var color: Color = .blue
+    public var body: some View {
+        
+        ZStack {
+            Color.clear
+                .ignoresSafeArea()
+            ForEach(0 ..< 5) { index in
+                Circle()
+                    .foregroundStyle(color)
+                    .frame(width: getCircleWidth(at: index))
+                    .offset(y: -frameWidth / 2)
+                    .rotationEffect(.degrees(isAnimating ? 360 : 0))
+                    .animation(.easeInOut(duration: 2-CGFloat(index)*0.2).delay(CGFloat(index)*0.2).repeatForever(autoreverses: false), value: isAnimating)
+            }
+        }
+       
+        .onAppear{
+            isAnimating = true
+        }
+        
+    }
+    
+    func getCircleWidth(at index: Int) -> CGFloat {
+        return frameWidth / 6 - CGFloat(index) * 1.5
+    }
+}
+
+#Preview {
+    LoaderAnimation()
+}
