7959f5084134776534b3322ba9522c2ed7b88030
Bilal Mughal <bilal@cinqtech.com>
2024-08-07 12:26:33 +0500

    init commit.

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "CustomLoader",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "CustomLoader",
+            targets: ["CustomLoader"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "CustomLoader"),
+        .testTarget(
+            name: "CustomLoaderTests",
+            dependencies: ["CustomLoader"]),
+    ]
+)
