4d2bba74de7a8610ce77172d19fa3449f20ac953
Bilal Mughal <bilal@cinqtech.com>
2024-08-16 11:53:13 +0500

    resolved.

--- Sources/CustomLoader/LoaderSwiftUI.swift
+++ Sources/CustomLoader/LoaderSwiftUI.swift
@@ -42,7 +42,7 @@ public struct LoaderSwiftUI: View {
                         .repeatForever(autoreverses: false), value: isAnimating)
             }
         }
-        .disabled(isAnimating)
+        .disabled(true)
         .onAppear{
             isAnimating = true
         }
